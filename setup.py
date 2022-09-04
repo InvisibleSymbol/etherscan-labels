4486bb6207ccdfd1345b579ae36db24f9c1e2df5
InvisibleSymbol <34715248+InvisibleSymbol@users.noreply.github.com>
2022-09-04 21:16:30 +0200

    update data dump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="etherscan_labels",
-    version="v20220423",
+    version="v20220904",
     author="InvisibleSymbol",
     author_email="etherscan-labels@invis.cloud",
     description="Easily queryable Labels from Etherscan",
